a6164e9f866c6ed1113f6965b201ccdf7c61148b
zayalachanze <zaya.gooding242@gmail.com>
2017-08-10 11:39:24 -0400

    Revamped collection view - unfinished

--- Gigster/Gigster/Controllers/SelectInstrumentsViewController.swift
+++ Gigster/Gigster/Controllers/SelectInstrumentsViewController.swift
@@ -2,17 +2,41 @@
 //  SelectInstrumentsViewController.swift
 //  Gigster
 //
-//  Created by Zaya Gooding on 8/9/17.
+//  Created by Zaya Gooding on 8/10/17.
 //  Copyright © 2017 Zaya Gooding. All rights reserved.
 //
 
 import UIKit
 
-class SelectInstrumentsViewController: UIViewController {
+final class SelectInstrumentsViewController: UICollectionViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: - Properties
+    fileprivate let reuseIdentifier = "InstrumentCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    let instrumentList = ["Vocals", "Acoustic Guitar", "Electric Guitar", "Piano", "Keyboard", "Bass Guitar", "Drums", "Ukulele", "Violin", "Viola", "Cello", "String/Upright Bass", "Saxophone", "Trumpet", "Trombone", "Tuba", "Oboe", "Flute", "Clarinet", "Organ", "Harp", "Xylophone", "Banjo", "Accordion", "Melodica", "Synth", "DJ/Midi"]
+}
+
+// MARK: - UICollectionViewDataSource
+extension SelectInstrumentsViewController {
+    //1
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return instrumentList.count
     }
     
-    static let instrumentList = ["Vocals", "Acoustic Guitar", "Electric Guitar", "Piano", "Keyboard", "Bass Guitar", "Drums", "Ukulele", "Violin", "Viola", "Cello", "String/Upright Bass", "Saxophone", "Trumpet", "Trombone", "Tuba", "Oboe", "Flute", "Clarinet", "Organ", "Harp", "Xylophone", "Banjo", "Accordion", "Melodica",  "DJ/Midi"]
+    //2
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return instrumentList.count
+    }
+    
+    //3
+    override func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath)
+        cell.backgroundColor = UIColor.black
+        // Configure the cell
+        return cell
+    }
 }
